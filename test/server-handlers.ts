2b3a1bc75853cc94a7caf1ed19386e53821efca8
Rocio Bottinelli <rociobottinelli@gmail.com>
2023-04-11 18:57:39 -0300

    Correct deploy error

--- test/server-handlers.ts
+++ test/server-handlers.ts
@@ -1,7 +1,7 @@
 import {rest} from 'msw'
 import comics from "dh-marvel/test/mocks/comics";
 import character from "dh-marvel/test/mocks/character";
-import comic from "dh-marvel/test/mocks/comic";
+import {comic} from "dh-marvel/test/mocks/comic";
 import comicsWithOffsetAndLimit from "dh-marvel/test/mocks/comicsWithOffsetAndLimit";
 import comicWithoutStock from "dh-marvel/test/mocks/comicWithoutStock";
 
